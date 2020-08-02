cd0c86dc63045aa2d49224e5dfed1d0bb7b48783
Dante Astudillo <daac99@gmail.com>
2020-08-01 22:09:50 -0500

    first commit

---
+++ src/main/java/com/mx/daac/RideApiApplication.java
@@ -0,0 +1,13 @@
+package com.mx.daac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideApiApplication.class, args);
+	}
+
+}
